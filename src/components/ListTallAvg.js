6c2e57c509edb9ab4c82e0d18a81ba1f0e55d6c8
Santiago13225 <69102034+Santiago13225@users.noreply.github.com>
2022-07-04 12:52:25 -0700

    New experimental code!

--- src/components/ListTallAvg.js
+++ src/components/ListTallAvg.js
@@ -115,7 +115,7 @@ const ListTallAvg = () => {
          console.log(employeeResponse.items.length);
          console.log(avgAge);
    
-         let t1 = "Aggregate Data for Males:\nAverage Age: ";
+         let t1 = "Aggregate Data for Tall Employees:\nAverage Age: ";
          let t2 = avgAge;
          let t3 = "\nAverage Height (*In inches): ";
          let t4 = avgHeight;
